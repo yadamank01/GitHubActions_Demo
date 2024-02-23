0d00a816866d74073ed9545f01c86de707abd022
NidaAP <smilykodi@gmail.com>
2024-02-23 09:53:53 +0530

    Add files via upload

---
+++ src/main/java/com/example/Calculator.java
@@ -0,0 +1,23 @@
+package com.example;
+
+public class Calculator {
+
+
+	public double doAdd(double num1, double num2) {
+		return num1 + num2;
+	}
+
+	public double doSub(double num1, double num2) {
+		//comment line
+		return num1 - num2;
+	}
+
+	public double doMul(double num1, double num2) {
+		return num1 * num2;
+	}
+
+	public double doDiv(double num1, double num2) {
+		return num1 / num2;
+	}
+
+}
---
+++ src/test/java/ut/CalculatorTest.java
@@ -0,0 +1,47 @@
+package ut;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.example.Calculator;
+
+
+public class CalculatorTest {
+
+static Calculator cl ;
+	
+	@BeforeClass
+	  public static void setUp() {
+	   cl = new Calculator();
+	  }
+
+	  @AfterClass
+	  public static void tearDown()  {
+	  System.gc();
+	  }
+
+	  @Test
+	  public void testAdd() {
+	 assertTrue(cl.doAdd(1,2)==3);
+	  }
+	  
+	  @Test
+	  public void testSub() {
+	 assertTrue(cl.doSub(1,1)==0);
+	  }
+	  
+	  @Test
+	  public void testMul() {
+	 assertTrue(cl.doMul(9,2)==18);
+	  }
+	  
+	  
+	  @Test
+	  public void testDiv() {
+	 assertTrue(cl.doDiv(8,2)==4);
+	  }
+
+}
